63aba1b5c2de8ff4e109020ea2014c00c5db9cb4
forbidden Betta <forbiddenBetta@gmail.com>
2022-01-27 16:23:42 +0100

    Avancée

--- src/Views/home.php
+++ src/Views/home.php
@@ -8,45 +8,43 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cat Bar</title>
-    /
 
-    <link rel="stylesheet" href="Assets/styles/style.css">
+    <link rel="stylesheet" href="Assets/Styles/style.css">
     <link rel="icon" href="Assets/cat.png" type="image/x-icon">
     <link rel="shortcut icon" href="Assets/cat.png" type="image/x-icon">
     <script src="/script.js"></script>
-
-
-
-
 </head>
 
 <body>
 
     <div class="mainbody">
+
         <header>
             <div class="banner_container">
                 <div class="banner_banner">
-                    <a href="disconnect.php">Deconnection</a>
-                    <a href="accounts.php">Comptes</a>
-                    <a href="https://cultofthepartyparrot.com/">News</a>
-                    <a href="categories.php">Categories</a>
-                    <a href="popular.php">Popular</a>
                     <a class="active" href="index.php">Accueil</a>
+                    <a href="accounts.php">Comptes</a>
+                    <a href="disconnect.php">Deconnection</a>
                 </div>
             </div>
         </header>
-        <div class="form_container">
-            <form method="post">
-                <input type="text" name="firstname" id="firstname" placeholder="firstname">
-                <input type="text" name="lastname" id="lastname" placeholder="lastname">
-                <input type="submit" name="send" value="envoyer">
-            </form>
+        <div class="mainbody_primary">
+            <div class="form_container">
+                <form method="post">
+                    <input type="text" name="firstname" id="firstname" placeholder="firstname">
+                    <input type="text" name="lastname" id="lastname" placeholder="lastname">
+                    <input type="submit" name="send" value="envoyer">
+                </form>
+            </div>
         </div>
     </div>
+    <footer>
+        Ce footer à été fait gratuitement. Il n'a pas été bien fait.
+        <br>
+        <a href="#">Apprenez en plus.</a>
+    </footer>
 </body>
 
-<footer>
 
-</footer>
 
 </html>
