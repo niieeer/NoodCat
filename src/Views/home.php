95eba16e2b0fadb4b69a8067a595afc7ba2874ed
forbidden Betta <forbiddenBetta@gmail.com>
2022-01-27 15:10:49 +0100

    Debut du CSS

---
+++ src/Views/home.php
@@ -0,0 +1,52 @@
+<!-- Via index je suppose -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Le meilleur bar à chats de l'Est!">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Bar</title>
+
+
+    <link rel="stylesheet" href="Assets/styles/style.css">
+    <link rel="icon" href="Assets/cat.png" type="image/x-icon">
+    <link rel="shortcut icon" href="Assets/cat.png" type="image/x-icon">
+    <script src="/script.js"></script>
+
+
+
+
+</head>
+
+<body>
+
+    <div class="mainbody">
+        <header>
+            <div class="banner_container">
+                <div class="banner_banner">
+                    <a href="disconnect.php">Deconnection</a>
+                    <a href="accounts.php">Comptes</a>
+                    <a href="https://cultofthepartyparrot.com/">News</a>
+                    <a href="categories.php">Categories</a>
+                    <a href="popular.php">Popular</a>
+                    <a class="active" href="index.php">Accueil</a>
+                </div>
+            </div>
+        </header>
+        <div class="form_container">
+            <form method="post">
+                <input type="text" name="firstname" id="firstname" placeholder="firstname">
+                <input type="text" name="lastname" id="lastname" placeholder="lastname">
+                <input type="submit" name="send" value="envoyer">
+            </form>
+        </div>
+    </div>
+</body>
+
+<footer>
+
+</footer>
+
+</html>
