4a255ea20515bfcf013fae796c0f2b96bc499491
niieeer <clement0201@icloud.com>
2022-01-27 15:02:16 +0100

    correction index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 use Router\Router;
 require_once('vendor/autoload.php');
 $router = new Router($_GET['url']);
-$router->get("/home", "App\Controller\AppController@index");
+$router->get("/", "App\Controller\AppController@index");
 $router->run();
 ?>
 
